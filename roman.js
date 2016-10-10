bb675c07c1bc86b2dfb2d5d00c115b4e58f18067
Jan Keromnes <janx@linux.com>
2016-10-10 23:35:41 +0200

    simplify code

--- roman.js
+++ roman.js
@@ -3,9 +3,9 @@
 var D = [1,5,10,50,100,500,1000], R = ['I','V','X','L','C','D','M'];
 
 exports.decimal = function(rom) {
-  var d = 0, i = 0, p = 0, c = 0;
-  while (i < rom.length) {
-    var p = D[R.indexOf(rom[i].toUpperCase())], c = (i<rom.length-1 ? D[R.indexOf(rom[i+1].toUpperCase())] : 0);
+  var d = 0, i = 0, p = 0, c = 0, r = rom.toUpperCase();
+  while (i < r.length) {
+    var p = D[R.indexOf(r[i])], c = (i < r.length-1 ? D[R.indexOf(r[i+1])] : 0);
     if (p < c) {d += c - p; i++;}
     else d += p;
     i++;
